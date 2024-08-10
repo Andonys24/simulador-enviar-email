07cd91dcad561482f7106fee90c320af746b212c
Andonys24 <andonihernandez1990@gmail.com>
2024-08-09 23:07:00 -0600

    - Creando de el objeto principar para validar     y sincronizar los datos - Habilitando o deshabilitando el boton de enviar

--- js/app.js
+++ js/app.js
@@ -1,28 +1,44 @@
 document.addEventListener("DOMContentLoaded", function () {
+	const datos = {
+		email: "",
+		asunto: "",
+		mensaje: "",
+	};
+
 	// Seleccionar los elementos de la interfaz
 	const inputEmail = document.querySelector("#email");
 	const inputAsunto = document.querySelector("#asunto");
 	const inputMensaje = document.querySelector("#mensaje");
-	const formulario = document.querySelector("#formulario");
+	const btnSubmit = document.querySelector('#formulario button[type="submit"]');
 
 	// Asignar eventos
-	inputEmail.addEventListener("blur", validarCampo);
-	inputAsunto.addEventListener("blur", validarCampo);
-	inputMensaje.addEventListener("blur", validarCampo);
+	inputEmail.addEventListener("input", validarCampo);
+	inputAsunto.addEventListener("input", validarCampo);
+	inputMensaje.addEventListener("input", validarCampo);
 
 	function validarCampo(e) {
 		const elementoPadre = e.target.parentElement;
 		if (e.target.value.trim() === "") {
 			mostrarAlerta(`El campo ${e.target.id} es obligatorio`, elementoPadre);
+			datos[e.target.id] = "";
+			comprobarDatos();
 			return;
 		}
 
 		if (e.target.id === "email" && !validarEmail(e.target.value)) {
 			mostrarAlerta("El Email no es válido", elementoPadre);
+			datos[e.target.id] = "";
+			comprobarDatos();
 			return;
 		}
 
 		limpiarAlerta(elementoPadre);
+
+		// Asignar los valores
+		datos[e.target.id] = e.target.value.trim().toLowerCase();
+
+		// Comprobar el objeto de email
+		comprobarDatos();
 	}
 
 	function mostrarAlerta(mensaje, referencia) {
@@ -51,5 +67,16 @@ document.addEventListener("DOMContentLoaded", function () {
 		const regex = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
 		const resultado = regex.test(email);
 		return resultado;
+	}
+
+	function comprobarDatos() {
+		if (Object.values(datos).includes("")) {
+			btnSubmit.classList.add("opacity-50");
+			btnSubmit.disabled = true;
+			return;
+		}
+
+		btnSubmit.classList.remove("opacity-50");
+		btnSubmit.disabled = false;
 	}
 });
