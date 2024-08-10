d4809aa0119bcf0f7ccef2d546f2a2a0ec7a35a4
Andonys24 <andonihernandez1990@gmail.com>
2024-08-09 21:50:29 -0600

    - Creando una funcion para validar - Detectando si un campo esta vacio

--- js/app.js
+++ js/app.js
@@ -3,4 +3,17 @@ document.addEventListener("DOMContentLoaded", function () {
 	const inputEmail = document.querySelector("#email");
 	const inputAsunto = document.querySelector("#asunto");
 	const inputMensaje = document.querySelector("#mensaje");
+
+	// Asignar eventos
+	inputEmail.addEventListener("blur", validarCampo);
+	inputAsunto.addEventListener("blur", validarCampo);
+	inputMensaje.addEventListener("blur", validarCampo);
+
+	function validarCampo(e) {
+		if (e.target.value.trim() === "") {
+			console.log("esta vacio...");
+		} else {
+			console.log("si hay algo...");
+		}
+	}
 });
